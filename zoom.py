9806745570f5061d01cdc3392b1e95ac8c566a66
Juliana <ju_jumc@hotmail.com>
2017-06-19 00:27:15 -0300

    Work in moving and merging frames

--- zoom.py
+++ zoom.py
@@ -13,46 +13,54 @@ def zoom(original, scale):
 
 	return zoomed
 
-def create_frames(layers, nframes, height, width):
-# TO DO: Move and merge layers	
+def create_frames(layers, nlayers, nframes, height, width):
+# Horizontal scroll
 	frames = []
-	z_height, z_width = layers[0].shape[:2]
+	z_height, z_width = layers[0][0].shape[:2]
+	cuts = []
+	alpha =  np.linspace(0.01, 0.5, nlayers) # 1 >= alpha > 0 : speed of movement
+	for i in range(nlayers):
+		cuts.append(np.linspace(0, (z_width - width)*alpha[i], nframes, dtype=int))
+	for i in range(nframes):
+		j = 0
+		curr_frame = layers[0][0]
+		for j in range(nlayers):
+			# Change(deslocate) cut values based on current frame index and layer number		
+			cut_w = int((z_width - width)/2)
+			cut_h = int((z_height - height)/2)
 
-	for i in xrange(nframes):
-		# Iterate over layers
-		# Change(deslocate) cut values based on current frame index and layer number
-		cut_w = ((z_width - width)/2).astype(int)
-		cut_h = ((z_height - height)/2).astype(int)
-		layer_frame = layer1[cut_h:-cut_h, cut_w:-cut_w]
-		# Merge acquired layer_frame and append to frames
+			if((cuts[j][i] - cut_w) >= 0):
+				beta = cuts[j][i]
+			else:
+				beta = cut_w
+			layer_frame = layers[j][0][cut_h:(-cut_h), cut_w - beta:-(cut_w + beta)]
+			# Merge acquired layer_frame and append to frames
+			cv2.addWeighted(curr_frame, 0.5, layers[j][0], 0.5, 0, curr_frame) # Problem: counting alpha as black
+		frames.append(curr_frame)
+		cv2.imshow(str(i),curr_frame)
+		cv2.waitKey(0)
+		cv2.imwrite("output_frames/frame"+str(i)+".png", curr_frame)
 
 	return frames
 
 def gif(imgs):
+#http://www.nooganeer.com/his/projects/image-processing/making-a-gif-with-opencv-and-scikit-image-in-python/
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_axis_off()
  
     ims = map(lambda x: (ax.imshow(x), ax.set_title('')), imgs)
     im_ani = animation.ArtistAnimation(fig, ims, interval=800, repeat_delay=0, blit=False)
-    
-    im_ani.save('parallax.gif')
     plt.show()
-
+    #im_ani.save('parallax.gif',writer="imagemagik")
 
 scale = 1.05
 nframes = 24
+nlayers = 5
 layers = []
 
-layer1 = cv2.imread("img/layer1.png", -1)
-layers.append([zoom(layer1, scale)])
-layer2 = cv2.imread("img/layer2.png", -1)
-layers.append([zoom(layer2, scale)])
-layer3 = cv2.imread("img/layer3.png", -1)
-layers.append([zoom(layer3, scale)])
-layer4 = cv2.imread("img/layer4.png", -1)
-layers.append([zoom(layer4, scale)])
-
-
-height, width = layer1.shape[:2]
-gif(create_frames(layers, nframes, height, width))
+for i in range(nlayers):
+	layer = cv2.imread("output_images/region"+str(i)+".png", -1)
+	layers.append([zoom(layer, scale)])
+height, width = layer.shape[:2]
+gif(create_frames(layers, nlayers, nframes, height, width))
